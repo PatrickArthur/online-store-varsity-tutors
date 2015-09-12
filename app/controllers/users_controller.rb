6b296001213b7725a86e5c77478a6d06ac3276ad
Patrick <pa@arthurfamily.ws>
2015-09-12 19:23:33 -0400

    added routes and user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+  before_filter :authenticate_user!
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    (@user.valid?) ? (new_user_valid?) : (new_user_not_valid?)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :email, :current_password, :password,
+      :password_confirmation, :first_name, :last_name,
+      :address, :city, :state, :country, :zipcode,
+      :phone_number_primary, :phone_number_secondary,
+      :terms_of_service, :age_verification
+    )
+  end
+
+  def new_user_valid?
+    flash[:notice] = 'Successful registration'
+    redirect_to products_path
+  end
+
+  def new_user_not_valid?
+    flash[:alert] = 'Errors, not registered'
+    render action: 'new'
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   require 'sidekiq/web'
   # ...
-  mount Sidekiq::Web, at: '/sidekiq'
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+
+  root to: "products#index"
+
+  resources :users, only: [:index, :show, :create]
+
+  resources :products, only: [:index, :show, :new, :create]
 end
